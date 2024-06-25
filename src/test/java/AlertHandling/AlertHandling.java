e80c41d5ca780e9e166d4bb3acbbc8f0a4a12d71
NigGitSai <niggit92@gmail.com>
2024-06-25 11:07:20 +0530

    Alert Handling and CalendarDatePicker test

---
+++ src/test/java/AlertHandling/AlertHandling.java
@@ -0,0 +1,118 @@
+package AlertHandling;
+
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.TimeUnit;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AlertHandling {
+static ChromeDriver driver;
+Alert alert;
+WebDriverWait wait;
+	
+	@BeforeClass
+	public void setUpBrowser()
+	{
+		WebDriverManager.chromedriver().setup(); 
+		 driver = new ChromeDriver();
+		 wait = new WebDriverWait(driver,10);
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@Test(description = "Simple Alert Verification", priority = 0)
+	public void simpleAlertValidation()
+	{
+		driver.get("https://letcode.in/alert");
+		WebElement acceptAlertBtn = driver.findElementById("accept");
+		wait.until(ExpectedConditions.elementToBeClickable(acceptAlertBtn));
+		acceptAlertBtn.click();
+		alert = driver.switchTo().alert();
+		System.out.println(alert.getClass());
+		alert.accept();
+	}
+	
+	@Test(description = "confirm Alert Verification",priority = 1)
+	public void confirmAlertValidation()
+	{
+		WebElement confirmBtn = driver.findElementById("confirm");
+		wait.until(ExpectedConditions.elementToBeClickable(confirmBtn));
+		confirmBtn.click();
+		
+		alert = driver.switchTo().alert();
+		System.out.println(alert.getClass());
+		System.out.println(alert.getText());
+		alert.dismiss();
+	}
+	
+	@Test(description = "Prompt Alert Verification",priority = 2)
+	public void promptAlertValidation()
+	{
+		WebElement promptBtn = driver.findElementById("prompt");
+		wait.until(ExpectedConditions.elementToBeClickable(promptBtn));
+		promptBtn.click();
+		wait.until(ExpectedConditions.alertIsPresent());
+		alert = driver.switchTo().alert();
+		System.out.println(alert.getClass());
+		
+		alert.sendKeys("Test");
+		alert.accept();
+		String actValue = driver.findElementById("myName").getText();
+		
+		org.testng.Assert.assertEquals(actValue, "Your name is: Test", "Prompt Alert verification");
+		
+	}
+	
+	@Test(description = "Web Popup Verification",priority = 3)
+	public void webPopupValidation()
+	{
+		WebElement modernBtn = driver.findElementById("modern");
+		wait.until(ExpectedConditions.elementToBeClickable(modernBtn));
+		modernBtn.click();
+		
+		boolean actFlag = driver.findElementByXPath("//p[contains(text(),'Modern Alert - Some people address me as sweet alert as well')]").isDisplayed();
+		driver.findElementByXPath("//button[@aria-label='close']").click();
+	
+		org.testng.Assert.assertEquals(actFlag, true, "Modern Alert popup is displayed");
+	
+	}
+
+	@Test(description = "Alert Screenshot Verification",priority = 4)
+	public void alertScreenshotValidation() throws AWTException, IOException
+	{
+		driver.findElementById("prompt").click();
+		alert = driver.switchTo().alert();
+		
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();		
+		Rectangle rect = new Rectangle(d);
+		Robot rb = new Robot();
+		BufferedImage io = rb.createScreenCapture(rect);
+		ImageIO.write(io, "png", new File("src\\test\\resources\\AlertRect.png"));
+		alert.accept();
+		
+		
+	}
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();	}
+}
---
+++ src/test/java/CalendarDatePicker/CalendarDatePickerTest.java
@@ -0,0 +1,81 @@
+package CalendarDatePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CalendarDatePickerTest {
+
+	@Test
+	public void setDateInCalendar() throws Exception
+	{
+		try
+		{
+		 WebDriverManager.chromedriver().setup(); 
+		 ChromeDriver driver = new ChromeDriver(); 
+		 
+		 driver.manage().window().maximize();
+		 driver.get("https://www.hyrtutorials.com/p/calendar-practice.html");
+		 
+		 String targetDate = "10/02/2023";
+		 SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+		 
+		 Date reqDate = format.parse(targetDate);
+		 format.setLenient(false);
+		 Calendar cal = Calendar.getInstance();
+		 cal.setTime(reqDate);
+		 int date = cal.get(Calendar.DAY_OF_MONTH);	
+		 
+		int targetMonth =  cal.get(Calendar.MONTH);	
+		int targetYear = cal.get(Calendar.YEAR);
+		
+		driver.findElement(By.id("second_date_picker")).click();
+		String currentDateMonth = driver.findElement(By.className("ui-datepicker-title")).getText();
+		SimpleDateFormat monthFormat = new SimpleDateFormat("MMM yyyy");
+		Date currentMonthDt =  monthFormat.parse(currentDateMonth);
+		cal.setTime(currentMonthDt);
+		 
+		int actMonth = cal.get(Calendar.MONTH);
+		int actYear = cal.get(Calendar.YEAR);
+		 JavascriptExecutor executor = (JavascriptExecutor)driver;
+		if(targetMonth< actMonth || targetYear < actYear)
+		{
+			WebElement prev = driver.findElement(By.xpath("//a[contains(@class,'ui-datepicker-prev')]"));
+			  executor.executeScript("arguments[0].click();", prev);
+			  actMonth = cal.get(Calendar.MONTH);
+			  actYear = cal.get(Calendar.YEAR);
+		  }
+		  while(targetMonth>actMonth || targetYear>actYear)
+		  {
+			  WebElement next =  driver.findElement(By.xpath("//a[contains(@class,'ui-datepicker-next')]"));
+			  executor.executeScript("arguments[0].click();", next);
+			  currentDateMonth = driver.findElement(By.className("ui-datepicker-title")).getText();
+			  cal.setTime(new SimpleDateFormat("MMM yyyy").parse(currentDateMonth));
+			  actMonth = cal.get(Calendar.MONTH);
+			  actYear = cal.get(Calendar.YEAR);
+		  }
+		  driver.findElement(By.xpath("//table[@class='ui-datepicker-calendar']//td[not(contains(@class,'ui-datepicker-other-month'))]//a[text()='"
+				  +date+"']")).click();
+		  System.out.println(date+" "+targetMonth+" "+targetYear);
+		  driver.quit();
+		}
+		catch(ParseException e)
+		{
+			throw new Exception("Invalid date change date input");
+		}
+		
+		 		 
+		 
+		 
+		 
+	}
+}
